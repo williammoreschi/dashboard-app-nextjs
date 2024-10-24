d200723c67783f6228ed3e4d05b882911aadfa09
william <william.moreschi@guiadomarceneiro.net>
2024-10-24 11:36:05 -0300

    Mostrando os dados na tela de Clientes

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,37 @@
+import { Suspense } from 'react';
 import { Metadata } from 'next';
+import Table from "@/app/ui/customers/table";
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import { CustomersTableSkeleton } from '@/app/ui/skeletons';
+import Search from '@/app/ui/search';
+import { lusitana } from '@/app/ui/fonts';
 
 export const metadata: Metadata = {
-  title: 'Costumers',
+  title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
+        Customers
+      </h1>
+      <Search placeholder="Search customers..." />
+      <Suspense key={query + currentPage} fallback={<CustomersTableSkeleton />}>
+        <Table customers={customers} />
+      </Suspense>
+    </div>
+  );
 }
